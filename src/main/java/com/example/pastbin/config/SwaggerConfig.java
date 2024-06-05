74f079ec088aa62778ef0e8624c949729a9173e9
Z <peak@gmail.com>
2024-06-05 12:06:12 +0300

    push project

--- src/main/java/com/example/pastbin/config/SwaggerConfig.java
+++ src/main/java/com/example/pastbin/config/SwaggerConfig.java
@@ -16,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     private static final String API_KEY = "Bearer Token";
@@ -38,5 +39,12 @@ public class SwaggerConfig {
                 .scheme("Bearer");
     }
 
-
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
